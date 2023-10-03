75f022c9326c0ff1945ee5ba6c6ef0d1855ab494
sherwin194 <vazsherwin71@gmail.com>
2023-10-04 01:08:48 +0530

    committed quick_sort.h

---
+++ quick_sort.h
@@ -0,0 +1,15 @@
+//quick sort header file
+
+#ifndef QUICK_SORT_H
+#define QUICK_SORT_H
+
+//initialize function for quick sort algorithm
+void quickSort(int arr[], int left, int right);
+
+//initialization of function
+void readInputFile(const char* filename, int arr[], int* size);
+
+//initialization of function
+void writeOutputFile(const char* filename, int arr[], int size);
+
+#endif // QUICK_SORT_H
