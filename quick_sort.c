0034bdb805670174f2a3e83a4ac113cb1738275d
sherwin194 <vazsherwin71@gmail.com>
2023-10-04 17:11:10 +0530

    updated quick_sort.c

--- quick_sort.c
+++ quick_sort.c
@@ -1,5 +1,6 @@
 //quick sort source file
 
+/*
 #include "quick_sort.h"
 #include <stdio.h>
 #include <stdlib.h>
@@ -57,6 +58,80 @@ void writeOutputFile(const char* filename, int arr[], int size) {
 
     for (int i = 0; i < size; i++) {
         fprintf(file, "%d\n", arr[i]);
+    }
+
+    fclose(file);
+}
+*/
+
+#include "quick_sort.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void quicksort(int arr[], char words[][100], int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, words, low, high);
+        quicksort(arr, words, low, pi - 1);
+        quicksort(arr, words, pi + 1, high);
+    }
+}
+
+int partition(int arr[], char words[][100], int low, int high) {
+    int pivot = arr[high];
+    int i = (low - 1);
+
+    for (int j = low; j <= high - 1; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            swap(&arr[i], &arr[j]);
+            // Swap corresponding words
+            char temp[100];
+            strcpy(temp, words[i]);
+            strcpy(words[i], words[j]);
+            strcpy(words[j], temp);
+        }
+    }
+    swap(&arr[i + 1], &arr[high]);
+    // Swap corresponding words
+    char temp[100];
+    strcpy(temp, words[i + 1]);
+    strcpy(words[i + 1], words[high]);
+    strcpy(words[high], temp);
+    return (i + 1);
+}
+
+void swap(int* a, int* b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void readCSV(const char* filename, int keys[], char words[][100], int* numEntries) {
+    FILE* file = fopen(filename, "r");
+    if (!file) {
+        perror("Error opening file");
+        exit(1);
+    }
+
+    int i = 0;
+    while (fscanf(file, "%d %[^\n]s", &keys[i], words[i]) != EOF) {
+        i++;
+    }
+
+    *numEntries = i;
+    fclose(file);
+}
+
+void writeCSV(const char* filename, int keys[], char words[][100], int numEntries) {
+    FILE* file = fopen(filename, "w");
+    if (!file) {
+        perror("Error opening file");
+        exit(1);
+    }
+
+    for (int i = 0; i < numEntries; i++) {
+        fprintf(file, "%d %s\n", keys[i], words[i]);
     }
 
     fclose(file);
