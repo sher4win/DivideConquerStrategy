033066e6a2ebd0dbab14d9e0125cfabd3d22a7ee
sherwin194 <vazsherwin71@gmail.com>
2023-10-04 17:04:59 +0530

    updated quick_sort.h

--- quick_sort.h
+++ quick_sort.h
@@ -1,5 +1,6 @@
 //quick sort header file
 
+/*
 #ifndef QUICK_SORT_H
 #define QUICK_SORT_H
 
@@ -13,3 +14,16 @@ void readInputFile(const char* filename, int arr[], int* size);
 void writeOutputFile(const char* filename, int arr[], int size);
 
 #endif // QUICK_SORT_H
+*/
+
+#ifndef QUICKSORT_H
+#define QUICKSORT_H
+
+void quicksort(int arr[], char words[][100], int low, int high);
+int partition(int arr[], char words[][100], int low, int high);
+void swap(int* a, int* b);
+void readCSV(const char* filename, int keys[], char words[][100], int* numEntries);
+void writeCSV(const char* filename, int keys[], char words[][100], int numEntries);
+
+#endif // QUICKSORT_H
+
